2dcddc72cfdd1517e6eccc7a1d10fc5fd71b0aa1
VanshSharma27 <2401010045@krmu.edu.in>
2026-04-24 18:24:53 +0530

    Update main.js

--- Assignment-4_TODO_2.0/static/js/main.js
+++ Assignment-4_TODO_2.0/static/js/main.js
@@ -1,10 +1,4 @@
-// ============================================================
-// main.js — Flask To-Do Application
-// Assignment 4 | SEC035 | B.Tech CSE | SOET
-// Student Name  : Aayush
-// Enrollment No.: 2401010042
-// Date          : April 2026
-// ============================================================
+
 
 document.addEventListener('DOMContentLoaded', function () {
     // Auto-dismiss flash messages after 3 seconds
